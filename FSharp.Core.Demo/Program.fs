7411c702572c3ef4f3518fa04c60c84fc06fd095
Yarn <ztytotoro@users.noreply.github.com>
2018-12-04 15:27:58 +0800

    test tuple in fsharp

--- FSharp.Core.Demo/Program.fs
+++ FSharp.Core.Demo/Program.fs
@@ -1,6 +1,16 @@
 ﻿open System
 
+type Color =
+    | 红色 = 1
+    | 绿色 = 2
+    | 黄色 = 3
+
+let test a b =
+    (a,b)
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    Console.WriteLine Color.红色
+    let (a, _) = test 1 2
+    Console.WriteLine a
+    Console.WriteLine (enum<Color> 3)
     0
